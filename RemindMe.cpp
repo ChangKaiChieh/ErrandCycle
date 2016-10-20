e874589364887736d6987c50a3e424769fe6858e
林大明 <cysh102210714@gmail.com>
2016-10-20 15:50:51 +0800

    Buidling 1

---
+++ RemindMe.cpp
@@ -0,0 +1,25 @@
+#include<cstdio>
+#include<iostream>
+#include<queue>
+#include<string>
+using namespace std;
+queue<string> Q1,Q2,Q3;
+void read(){
+    FILE *f;
+	f=fopen("data.txt","r");
+
+    return;
+}
+void write(){
+
+}
+int main(){
+    read();
+    show_instru();
+    char s[100];
+    while(scanf("%s",s)!=EOF){
+        switch s{
+        }
+    }
+    return 0;
+}
